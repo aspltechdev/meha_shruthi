06290d24df7ee1c86df890c7d930230c2f72c528
Akshay <akshaydamera407@gmail.com>
2026-06-12 17:24:42 +0530

    feat: add SocialWall component and SEO meta tags to index.html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Artists from "../components/Artists/Artists";
 import Gallery from "../components/Gallery/Gallery";
 import Sponsors from "../components/Sponsors/Sponsors";
 import Testimonials from "../components/Testimonials/Testimonials";
-import CTA from "../components/CTA/CTA";
+import CTA from "../components/CTA/CTA"; 
 import Footer from "../components/Footer/Footer";
 import LegacySection from "../components/LegacySection";
 import MeetTheVoices from "../components/MeetTheVoices";
@@ -20,6 +20,7 @@ import TrustedBy from "../components/TrustedBy";
 import ClientReactions from "../components/ClientReactions";
 import FinalCTA from "../components/FinalCTA";
 import ContactForm from "../components/ContactForm";
+import SocialWall from "../components/SocialWall";
 
 const Home = () => {
   return (
@@ -41,6 +42,7 @@ const Home = () => {
         <EventJourney/>
         <TrustedBy/>
         <ClientReactions/>
+        <SocialWall/>
         <FinalCTA/>
         <ContactForm/>
       </section>
